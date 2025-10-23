03545b7834d378da7561d8b0cf8190c9534a9208
sarasantosdev <contatosaracsantos@gmail.com>
2025-10-23 16:23:30 -0300

    feat(routes): criar rotas para os métodos GET, POST, PUT, PATCH e DELETE

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userControllers');
+
+router.get('/', userController.listUsers);
+
+router.get('/:id', userController.getUserById);
+
+router.post('/', userController.createUser);
+
+router.put('/:id', userController.updateUser);
+
+router.patch('/:id', userController.patchUser);
+
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
