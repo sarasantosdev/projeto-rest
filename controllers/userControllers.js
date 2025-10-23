2547444d34fb3b6b481bc93c1513fac4f94fd489
sarasantosdev <contatosaracsantos@gmail.com>
2025-10-23 16:22:48 -0300

    feat(controller): criar funções para manipulação dos dados

---
+++ controllers/userControllers.js
@@ -0,0 +1,65 @@
+const users = require('../data/users');
+
+exports.listUsers = (req, res) => {
+    res.json(users);
+};
+
+exports.getUserById = (req, res) =>{
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id == id);
+
+    if(!user){
+        return res.status(404).json({message: 'Usuário não encontrado'});
+    }
+
+    res.json(user);
+};
+
+exports.createUser = (req, res) => {
+    const newUser = req.body;
+    const newId = users.length ? Math.max(...users.map( u => u.id)) + 1 : 1;
+
+    const user = {id: newId, ...newUser};
+    users.push(user);
+
+    res.status(201).json(user);
+};
+
+exports.updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(u => u.id === id);
+
+    if (index === -1){
+        return res.status(404).json({message: 'Usuário não encontrado'});
+    }
+
+    users[index] = {id, ...req.body};
+
+    res.json(users[index]);
+};
+
+exports.patchUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id == id);
+
+    if(!user){
+       return res.status(404).json({message: 'Usuário não encontrado'});
+    }
+
+    Object.assign(user, req.body);
+
+    res.json(user);
+};
+
+exports.deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(u => u.id === id);
+
+    if (index === -1){
+        res.status(404).json({message: 'Usuário não encontrado'});
+    };
+
+    users.splice(index, 1);
+
+    res.status(204).send();
+};
