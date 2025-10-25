e8484c11f1e5bac5078b1d39467baf081d29df07
sarasantosdev <contatosaracsantos@gmail.com>
2025-10-24 23:50:36 -0300

    feat(routes): alterar na rota 1ª rota get a função listUsers para getUsers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 
-router.get('/', userController.listUsers);
+router.get('/', userController.getUsers);
 
 router.get('/:id', userController.getUserById);
 
