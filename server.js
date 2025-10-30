84a932e2796e6800c3b5d4524da351446eec921e
sarasantosdev <contatosaracsantos@gmail.com>
2025-10-30 19:44:27 -0300

    feat(server): importar client prisma

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import dotenv from 'dotenv';
+import prisma from '../projeto-rest/generated/prisma/client.js'; 
 dotenv.config();
 
 const app = express();
@@ -13,3 +14,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT)
     .on('listening', () => console.log(`Executando servidor na porta ${PORT}`))
     .on('error', (err) => console.error('Falha ao iniciar o servidor', err));
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  console.log('🧹 Conexão com o banco encerrada.');
+  process.exit(0);
+});
